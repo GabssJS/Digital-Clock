be39e79927fa263cef905b7280b6fc583f941692
GabssJS <gabriel04asr@gmail.com>
2023-05-18 14:34:13 -0300

    Revert "Fixed issue #4"
    This reverts commit 5f047cddc52d7af3d5b1474fcdd0952664fa2021.
    
    Commited to the wrong branch

--- src/Clock.jsx
+++ src/Clock.jsx
@@ -2,15 +2,13 @@ import HourInfo from './components/HourInfo/HourInfo.jsx'
 
 const Clock = () => {
     return(
-        <>
-            <div className="d-flex flex-column flex-sm-row gap-2 justify-content-center align-items-center contentContainer p-sm-5">
-                <HourInfo info="horas"/>
-                <span className="display-5">:</span>
-                <HourInfo info="minutos"/>
-                <span className="display-5">:</span>
-                <HourInfo info="segundos"/>
-            </div>
-        </>
+        <div className="d-flex flex-column flex-sm-row gap-2 justify-content-center align-items-center contentContainer p-sm-5">
+            <HourInfo info="horas"/>
+            <span className="display-5">:</span>
+            <HourInfo info="minutos"/>
+            <span className="display-5">:</span>
+            <HourInfo info="segundos"/>
+        </div>
     )
 }
 
