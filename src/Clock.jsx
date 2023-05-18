8042be59798b61df3836bde076f40600caefd023
GabssJS <gabriel04asr@gmail.com>
2023-05-18 17:11:37 -0300

    added toggle for 24h / am-pm formats

--- src/Clock.jsx
+++ src/Clock.jsx
@@ -1,14 +1,28 @@
+import { useRef, useState } from 'react'
 import HourInfo from './components/HourInfo/HourInfo.jsx'
 
 const Clock = () => {
+
+    const [format, setFormat] = useState()
+
+    function handleClickAm() {
+        setFormat("AM")
+    }
+
+    function handleClickTwnFr() {
+        setFormat("TwnFr")
+    }
+
     return(
-        <div className="d-flex flex-column flex-sm-row gap-2 justify-content-center align-items-center contentContainer p-sm-5">
-            <HourInfo info="horas"/>
-            <span className="display-5">:</span>
-            <HourInfo info="minutos"/>
-            <span className="display-5">:</span>
-            <HourInfo info="segundos"/>
+    <>
+        <div className="d-flex justify-content-center align-items-center contentContainer">
+            <HourInfo firstBox="horas" secondBox="minutos" thirdBox="segundos" format={format}/>
+            <div className="position-absolute buttonWrapper d-flex gap-3 w-100 justify-content-center align-items-center">
+                <button onClick={() => handleClickAm()} className=" border altButton rounded">AM/PM</button>
+                <button onClick={() => handleClickTwnFr()} className=" border altButton rounded">24h</button>
+            </div>
         </div>
+    </>
     )
 }
 
--- src/components/HourInfo/HourInfo.jsx
+++ src/components/HourInfo/HourInfo.jsx
@@ -4,33 +4,52 @@ import { useEffect, useState } from 'react'
 const HourInfo = (props) => {
 
     const [date, setDate] = useState(new Date())
-    var timeInfo = 0
+    const [dayPeriod, setDayPeriod] = useState("")
+
+    var hours = date.getHours()
+    var minutes = date.getMinutes()
+    var seconds = date.getSeconds()
 
     useEffect(() => {
         const timerId = setInterval(function refreshDate() {
             setDate(new Date())
 
         }, 1000)
+
+        if (hours < 12) {
+            setDayPeriod("AM")
+        }
+        else {
+            setDayPeriod("PM")
+        }
+
         return function cleanup() {
             clearInterval(timerId)
         }
     }, [])
 
-    if (props.info === 'horas') {
-        timeInfo = date.getHours()
-    }
-    else if (props.info === 'minutos') {
-        timeInfo = date.getMinutes()
-    }
-    else {
-        timeInfo = date.getSeconds()
-    }
+    
 
     return (
-        <div className="d-inline-flex flex-column justify-content-center align-items-center border rounded smHourContainer HourContainer">
-            <h1 className="display-1 hourNumber">{timeInfo/ 10 >= 1 ? timeInfo : '0' + timeInfo}</h1>
-            <span className="display-5 HourInfo text-muted">{props.info}</span>
+        <>
+        <div className="w-100 d-flex flex-row justify-content-center align-items-center gap-2 gap-sm-4">
+            <div className="d-inline-flex flex-column justify-content-center align-items-center border rounded HourContainer">
+                <h1 className="display-1 hourNumber">{props.format == "AM" ? "0" + (hours - 12) : hours / 10 >= 1 ? hours : "0" + hours}</h1>
+                <span className="display-5 HourInfo text-muted">{props.firstBox}</span>
+            </div>
+            <span className="display-5">:</span>
+            <div className="d-inline-flex flex-column justify-content-center align-items-center border rounded HourContainer">
+                <h1 className="display-1 hourNumber">{minutes / 10 >= 1 ? minutes : '0' + minutes}</h1>
+                <span className="display-5 HourInfo text-muted">{props.secondBox}</span>
+            </div>
+            <span className="display-5">:</span>
+            <div className="d-inline-flex flex-column justify-content-center align-items-center border rounded HourContainer">
+                <h1 className="display-1 hourNumber">{seconds / 10 >= 1 ? seconds : '0' + seconds}</h1>
+                <span className="display-5 HourInfo text-muted">{props.thirdBox}</span>
+            </div>
+            {props.format == "AM" && <span className="display-5">{dayPeriod}</span>}
         </div>
+        </>
     )
 }
 
